3b19dc6fa1f3678976a63900767e04dfdbcc11d7
mfelipegs <matheusg.dasilva7@gmail.com>
2024-02-16 17:58:41 -0300

    chore: adjusting train script and dataset cleanup

--- train.py
+++ train.py
@@ -1,34 +1,22 @@
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.models import load_model
 
-# Definir o modelo
-model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
-    MaxPooling2D((2, 2)),
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D((2, 2)),
-    Conv2D(128, (3, 3), activation='relu'),
-    MaxPooling2D((2, 2)),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dense(3, activation='softmax')  # 3 classes: ora-pro-nóbis, inhame, hibisco
-])
-
-# Compilar o modelo
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+# Carregar modelo
+model = load_model('models/yam.h5')
 
 # Definir gerador de dados para treinamento
 train_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(
         'pre_processed_dataset/train',
         target_size=(224, 224),
-        batch_size=32)
+        batch_size=32,
+        class_mode='categorical')
 
-print(len(train_generator))
+# Compilar o modelo
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 # Treinar o modelo
 model.fit(train_generator, epochs=10)
