9c38157e19907117b04f904a4ed1fec83035f273
mfelipegs <matheusg.dasilva7@gmail.com>
2024-05-16 15:51:56 -0300

    feat: api image processor

--- train.py
+++ train.py
@@ -1,12 +1,20 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.callbacks import ModelCheckpoint
 import numpy as np
-from keras.src.legacy.preprocessing.image import ImageDataGenerator
-from keras import Sequential
-from keras.src.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from keras.src.callbacks import ModelCheckpoint
+
+# Definir o caminho para o conjunto de dados
+train_data_dir = 'pre_processed_dataset5/train'
+validation_data_dir = 'pre_processed_dataset5/validation'
+
+# Definir o tamanho das imagens e o número de classes
+img_width, img_height = 224, 224
+num_classes = 3
 
 # Criar o modelo
 model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+    Conv2D(32, (3, 3), activation='relu', input_shape=(img_width, img_height, 3)),
     MaxPooling2D((2, 2)),
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D((2, 2)),
@@ -14,7 +22,7 @@ model = Sequential([
     MaxPooling2D((2, 2)),
     Flatten(),
     Dense(128, activation='relu'),
-    Dense(3, activation='softmax')  # 3 classes: ora-pro-nóbis, inhame, hibisco
+    Dense(num_classes, activation='softmax')
 ])
 
 # Compilar o modelo
@@ -22,27 +30,44 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Definir um gerador de dados para treinamento
-train_datagen = ImageDataGenerator(rescale=1./255)
-train_generator = train_datagen.flow_from_directory(
-        'dataset/train',
-        target_size=(224, 224),
-        batch_size=32,
-        class_mode='sparse')  # Não é necessário especificar num_classes
+# Criar geradores de dados para treinamento e validação
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
 
-# Definir um gerador de dados para validação
 validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=32,
+    class_mode='sparse'
+)
+
 validation_generator = validation_datagen.flow_from_directory(
-        'dataset/validation',
-        target_size=(224, 224),
-        batch_size=32,
-        class_mode='sparse')  # Não é necessário especificar num_classes
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=32,
+    class_mode='sparse'
+)
 
 # Definir um callback para salvar o modelo periodicamente durante o treinamento
-checkpoint = ModelCheckpoint('models/pancs_checkpoint.keras', save_best_only=True)
+checkpoint = ModelCheckpoint('modelsv7/pancs_checkpoint.keras', save_best_only=True)
 
 # Treinar o modelo com dados de validação
-model.fit(train_generator, epochs=10, callbacks=[checkpoint], validation_data=validation_generator)
+model.fit(
+    train_generator,
+    epochs=20,
+    callbacks=[checkpoint],
+    validation_data=validation_generator
+)
 
 # Salvar o modelo treinado
-model.save('models/pancs.keras')
+model.save('modelsv7/pancs.keras')
