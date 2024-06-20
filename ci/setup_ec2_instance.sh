98999e1c157f50edb394f71d29d584f191c45a2e
Matheus Felipe Gomes <104948713+mfelipegs@users.noreply.github.com>
2024-06-20 15:42:29 -0300

    build: update setup_ec2_instance.sh script

--- ci/setup_ec2_instance.sh
+++ ci/setup_ec2_instance.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-# Atualiza os pacotes e instala o Git LFS
-sudo yum update -y
-sudo yum install git-lfs -y
+# Atualiza os pacotes e instala o Git e o Git LFS
+sudo apt update
+sudo apt install git -y
+sudo apt install git-lfs -y
 
-# Instala o Python 3 e suas dependências
-sudo yum install python3 -y
-sudo yum install python3-pip -y
-pip3 install flask requests pillow
+# Instala o Python 3 e venv
+sudo apt install python3 -y
+sudo apt install python3-venv -y
 
-# Instala o Keras e o TensorFlow
-echo "Instalando Keras..."
-pip3 install keras==3.3
-echo "Instalando TensorFlow..."
-pip3 install tensorflow==2.16.1 --no-cache-dir
-
-# Alocando memória do swap
+# Alocando memória do swap (opcional, apenas se necessário)
 echo "Alocando memória do swap..."
 sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
@@ -23,6 +17,15 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 sudo swapon --show
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Cria e ativa um ambiente virtual Python
+echo "Criando e ativando ambiente virtual Python..."
+python3 -m venv myenv
+source myenv/bin/activate
+
+# Instala os pacotes no ambiente virtual
+echo "Instalando pacotes..."
+pip install flask requests pillow keras==3.3 tensorflow==2.16.1
 
 # Clona o repositório do Git com Git LFS
 echo "Instalando o projeto..."
@@ -32,4 +35,4 @@ cd Osiris-panc-classification-ai
 
 # Executa o projeto
 echo "Executando o projeto..."
-python3 app.py
+nohup python3 app.py > output.log 2>&1 &
